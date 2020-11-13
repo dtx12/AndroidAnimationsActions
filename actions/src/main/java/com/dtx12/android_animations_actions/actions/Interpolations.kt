6be55281753c5971d231f93209f7eafd24a7fc65
Jan Rabe <cloudgazer3d@gmail.com>
2020-11-13 18:54:21 +0100

    converted to sealed classes

--- actions/src/main/java/com/dtx12/android_animations_actions/actions/Interpolations.kt
+++ actions/src/main/java/com/dtx12/android_animations_actions/actions/Interpolations.kt
@@ -1,6 +1,10 @@
 package com.dtx12.android_animations_actions.actions
 
 import android.view.animation.Interpolator
+import kotlin.math.cos
+import kotlin.math.pow
+import kotlin.math.sin
+import kotlin.math.sqrt
 
 /*
  * Timing functions. Based on https://github.com/raywenderlich/SKTUtils/blob/master/SKTUtils/SKTTimingFunctions.swift
@@ -25,148 +29,227 @@ import android.view.animation.Interpolator
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-object Interpolations {
-    val QuadraticEaseIn = Interpolator { t -> t * t }
-    val QuadraticEaseOut = Interpolator { t -> t * (2.0f - t) }
-    val QuadraticEaseInOut = Interpolator { t ->
-        if (t < 0.5) {
+sealed class Interpolations : Interpolator {
+
+    object QuadraticEaseIn : Interpolations() {
+        override fun getInterpolation(t: Float): Float = t * t
+    }
+
+    object QuadraticEaseOut : Interpolations() {
+        override fun getInterpolation(t: Float): Float = t * (2.0f - t)
+    }
+
+    object QuadraticEaseInOut : Interpolations() {
+        override fun getInterpolation(t: Float): Float = if (t < 0.5) {
             2.0f * t * t
         } else {
             val f = t - 1.0f
             1.0f - 2.0f * f * f
         }
     }
-    val CubicEaseIn = Interpolator { t -> t * t * t }
-    val CubicEaseOut = Interpolator { t ->
-        val f = t - 1.0f
-        1.0f + f * f * f
+
+    object CubicEaseIn : Interpolations() {
+        override fun getInterpolation(t: Float): Float = t * t * t
     }
-    val CubicEaseInOut = Interpolator { t ->
-        if (t < 0.5) {
+
+    object CubicEaseOut : Interpolations() {
+        override fun getInterpolation(t: Float): Float {
+            val f = t - 1.0f
+            return 1.0f + f * f * f
+        }
+    }
+
+    object CubicEaseInOut : Interpolations() {
+        override fun getInterpolation(t: Float): Float = if (t < 0.5) {
             4.0f * t * t * t
         } else {
             val f = t - 1.0f
             1.0f + 4.0f * f * f * f
         }
     }
-    val QuarticEaseIn = Interpolator { t -> t * t * t * t }
-    val QuarticEaseOut = Interpolator { t ->
-        val f = t - 1.0f
-        1.0f - f * f * f * f
+
+    object QuarticEaseIn : Interpolations() {
+        override fun getInterpolation(t: Float): Float = t * t * t * t
     }
-    val QuarticEaseInOut = Interpolator { t ->
-        if (t < 0.5) {
+
+    object QuarticEaseOut : Interpolations() {
+        override fun getInterpolation(t: Float): Float {
+            val f = t - 1.0f
+            return 1.0f - f * f * f * f
+        }
+    }
+
+    object QuarticEaseInOut : Interpolations() {
+        override fun getInterpolation(t: Float): Float = if (t < 0.5) {
             8.0f * t * t * t * t
         } else {
             val f = t - 1.0f
             1.0f - 8.0f * f * f * f * f
         }
     }
-    val FunctionQuinticEaseIn = Interpolator { t -> t * t * t * t * t }
-    val QuinticEaseOut = Interpolator { t ->
-        val f = t - 1.0f
-        1.0f + f * f * f * f * f
+
+    object FunctionQuinticEaseIn : Interpolations() {
+        override fun getInterpolation(t: Float): Float = t * t * t * t * t
     }
-    val QuinticEaseInOut = Interpolator { t ->
-        if (t < 0.5) {
+
+    object QuinticEaseOut : Interpolations() {
+        override fun getInterpolation(t: Float): Float {
+            val f = t - 1.0f
+            return 1.0f + f * f * f * f * f
+        }
+    }
+
+    object QuinticEaseInOut : Interpolations() {
+        override fun getInterpolation(t: Float): Float = if (t < 0.5) {
             16.0f * t * t * t * t * t
         } else {
             val f = t - 1.0f
             1.0f + 16.0f * f * f * f * f * f
         }
     }
-    val SineEaseIn = Interpolator { t -> (Math.sin((t - 1.0f) * Math.PI / 2) + 1.0f).toFloat() }
-    val SineEaseOut = Interpolator { t -> Math.sin(t * Math.PI / 2).toFloat() }
-    val SineEaseInOut = Interpolator { t -> (0.5f * (1.0f - Math.cos(t * Math.PI))).toFloat() }
-    val CircularEaseIn = Interpolator { t -> (1.0f - Math.sqrt(1.0 - t * t)).toFloat() }
-    val CircularEaseOut = Interpolator { t -> Math.sqrt((2.0f - t) * t.toDouble()).toFloat() }
-    val CircularEaseInOut = Interpolator { t ->
-        if (t < 0.5) {
-            (0.5 * (1.0 - Math.sqrt(1.0 - 4.0 * t * t))).toFloat()
+
+    object SineEaseIn : Interpolations() {
+        override fun getInterpolation(t: Float): Float = (sin((t - 1.0f) * Math.PI / 2) + 1.0f).toFloat()
+    }
+
+    object SineEaseOut : Interpolations() {
+        override fun getInterpolation(t: Float): Float = sin(t * Math.PI / 2).toFloat()
+    }
+
+    object SineEaseInOut : Interpolations() {
+        override fun getInterpolation(t: Float): Float = (0.5f * (1.0f - cos(t * Math.PI))).toFloat()
+    }
+
+    object CircularEaseIn : Interpolations() {
+        override fun getInterpolation(t: Float): Float = (1.0f - sqrt(1.0 - t * t)).toFloat()
+    }
+
+    object CircularEaseOut : Interpolations() {
+        override fun getInterpolation(t: Float): Float = sqrt((2.0f - t) * t.toDouble()).toFloat()
+    }
+
+    object CircularEaseInOut : Interpolations() {
+        override fun getInterpolation(t: Float): Float = if (t < 0.5) {
+            (0.5 * (1.0 - sqrt(1.0 - 4.0 * t * t))).toFloat()
         } else {
-            (0.5 * Math.sqrt(-4.0 * t * t + 8.0 * t - 3.0) + 0.5).toFloat()
+            (0.5 * sqrt(-4.0 * t * t + 8.0 * t - 3.0) + 0.5).toFloat()
         }
     }
-    val ExponentialEaseIn = Interpolator { t -> if (t.toDouble() == 0.0) t else Math.pow(2.0, 10.0 * (t - 1.0)).toFloat() }
-    val ExponentialEaseOut = Interpolator { t -> if (t.toDouble() == 1.0) t else (1.0 - Math.pow(2.0, -10.0 * t)).toFloat() }
-    val ExponentialEaseInOut = Interpolator { t ->
-        if (t.toDouble() == 0.0 || t.toDouble() == 1.0) {
+
+    object ExponentialEaseIn : Interpolations() {
+        override fun getInterpolation(t: Float): Float = if (t.toDouble() == 0.0) t else 2.0.pow(10.0 * (t - 1.0)).toFloat()
+    }
+
+    object ExponentialEaseOut : Interpolations() {
+        override fun getInterpolation(t: Float): Float = if (t.toDouble() == 1.0) t else (1.0 - 2.0.pow(-10.0 * t)).toFloat()
+    }
+
+    object ExponentialEaseInOut : Interpolations() {
+        override fun getInterpolation(t: Float): Float = if (t.toDouble() == 0.0 || t.toDouble() == 1.0) {
             t
         } else if (t < 0.5) {
-            (0.5 * Math.pow(2.0, 20.0 * t - 10.0)).toFloat()
+            (0.5 * 2.0.pow(20.0 * t - 10.0)).toFloat()
         } else {
-            (1.0 - 0.5 * Math.pow(2.0, -20.0 * t + 10.0)).toFloat()
+            (1.0 - 0.5 * 2.0.pow(-20.0 * t + 10.0)).toFloat()
         }
     }
-    val ElasticEaseIn = Interpolator { t -> (Math.sin(13.0 * Math.PI / 2 * t) * Math.pow(2.0, 10.0 * (t - 1.0))).toFloat() }
-    val ElasticEaseOut = Interpolator { t -> (Math.sin(-13.0 * Math.PI / 2 * (t + 1.0)) * Math.pow(2.0, -10.0 * t) + 1.0).toFloat() }
-    val ElasticEaseInOut = Interpolator { t ->
-        if (t < 0.5) {
-            (0.5 * Math.sin(13.0 * Math.PI * t) * Math.pow(2.0, 20.0 * t - 10.0)).toFloat()
+
+    object ElasticEaseIn : Interpolations() {
+        override fun getInterpolation(t: Float): Float = (sin(13.0 * Math.PI / 2 * t) * 2.0.pow(10.0 * (t - 1.0))).toFloat()
+    }
+
+    object ElasticEaseOut : Interpolations() {
+        override fun getInterpolation(t: Float): Float = (sin(-13.0 * Math.PI / 2 * (t + 1.0)) * 2.0.pow(-10.0 * t) + 1.0).toFloat()
+    }
+
+    object ElasticEaseInOut : Interpolations() {
+        override fun getInterpolation(t: Float): Float = if (t < 0.5) {
+            (0.5 * sin(13.0 * Math.PI * t) * 2.0.pow(20.0 * t - 10.0)).toFloat()
         } else {
-            (0.5 * Math.sin(-13.0 * Math.PI * t) * Math.pow(2.0, -20.0 * t + 10.0) + 1.0).toFloat()
+            (0.5 * sin(-13.0 * Math.PI * t) * 2.0.pow(-20.0 * t + 10.0) + 1.0).toFloat()
         }
     }
-    val BackEaseIn = Interpolator { t ->
-        val s = 1.70158f
-        ((s + 1.0f) * t - s) * t * t
-    }
-    val BackEaseOut = Interpolator { t ->
-        val s = 1.70158f
-        val f = 1.0f - t
-        1.0f - ((s + 1.0f) * f - s) * f * f
-    }
-    val BackEaseInOut = Interpolator { t ->
-        val s = 1.70158f
-        if (t < 0.5) {
-            val f = 2.0f * t
-            0.5f * ((s + 1.0f) * f - s) * f * f
-        } else {
-            val f = 2.0f * (1.0f - t)
-            1.0f - 0.5f * ((s + 1.0f) * f - s) * f * f
-        }
-    }
-    val ExtremeBackEaseIn = Interpolator { t -> ((t * t - Math.sin(t * Math.PI)) * t).toFloat() }
-    val ExtremeBackEaseOut = Interpolator { t ->
-        val f = 1.0f - t
-        (1.0 - (f * f - Math.sin(f * Math.PI)) * f).toFloat()
-    }
-    val ExtremeBackEaseInOut = Interpolator { t ->
-        if (t < 0.5) {
-            val f = 2.0f * t
-            (0.5 * (f * f - Math.sin(f * Math.PI)) * f).toFloat()
-        } else {
-            val f = 2.0f * (1.0f - t)
-            (1.0 - 0.5 * (f * f - Math.sin(f * Math.PI)) * f).toFloat()
-        }
-    }
-    val BounceEaseOut = Interpolator { t ->
-        if (t < 1.0 / 2.75) {
-            7.5625f * t * t
-        } else if (t < 2.0 / 2.75) {
-            val f = t - 1.5f / 2.75f
-            7.5625f * f * f + 0.75f
-        } else if (t < 2.5 / 2.75) {
-            val f = t - 2.25f / 2.75f
-            7.5625f * f * f + 0.9375f
-        } else {
-            val f = t - 2.625f / 2.75f
-            7.5625f * f * f + 0.984375f
-        }
-    }
-    val BounceEaseIn: Interpolator = object : Interpolator {
+
+    object BackEaseIn : Interpolations() {
         override fun getInterpolation(t: Float): Float {
-            return 1.0f - getInterpolation(1.0f - t)
+            val s = 1.70158f
+            return ((s + 1.0f) * t - s) * t * t
         }
     }
-    val BounceEaseInOut: Interpolator = object : Interpolator {
+
+    object BackEaseOut : Interpolations() {
         override fun getInterpolation(t: Float): Float {
+            val s = 1.70158f
+            val f = 1.0f - t
+            return 1.0f - ((s + 1.0f) * f - s) * f * f
+        }
+    }
+
+    object BackEaseInOut : Interpolations() {
+        override fun getInterpolation(t: Float): Float {
+            val s = 1.70158f
             return if (t < 0.5) {
-                0.5f * getInterpolation(t * 2.0f)
+                val f = 2.0f * t
+                0.5f * ((s + 1.0f) * f - s) * f * f
             } else {
-                0.5f * getInterpolation(t * 2.0f - 1.0f) + 0.5f
+                val f = 2.0f * (1.0f - t)
+                1.0f - 0.5f * ((s + 1.0f) * f - s) * f * f
             }
         }
     }
-    val SmoothStep = Interpolator { t -> t * t * (3 - 2 * t) }
+
+    object ExtremeBackEaseIn : Interpolations() {
+        override fun getInterpolation(t: Float): Float = ((t * t - sin(t * Math.PI)) * t).toFloat()
+    }
+
+    object ExtremeBackEaseOut : Interpolations() {
+        override fun getInterpolation(t: Float): Float {
+            val f = 1.0f - t
+            return (1.0 - (f * f - sin(f * Math.PI)) * f).toFloat()
+        }
+    }
+
+    object ExtremeBackEaseInOut : Interpolations() {
+        override fun getInterpolation(t: Float): Float = if (t < 0.5) {
+            val f = 2.0f * t
+            (0.5 * (f * f - sin(f * Math.PI)) * f).toFloat()
+        } else {
+            val f = 2.0f * (1.0f - t)
+            (1.0 - 0.5 * (f * f - sin(f * Math.PI)) * f).toFloat()
+        }
+    }
+
+    object BounceEaseOut : Interpolations() {
+        override fun getInterpolation(t: Float): Float = when {
+            t < 1.0 / 2.75 -> 7.5625f * t * t
+            t < 2.0 / 2.75 -> {
+                val f = t - 1.5f / 2.75f
+                7.5625f * f * f + 0.75f
+            }
+            t < 2.5 / 2.75 -> {
+                val f = t - 2.25f / 2.75f
+                7.5625f * f * f + 0.9375f
+            }
+            else -> {
+                val f = t - 2.625f / 2.75f
+                7.5625f * f * f + 0.984375f
+            }
+        }
+    }
+
+    object BounceEaseIn : Interpolations() {
+        override fun getInterpolation(t: Float): Float = 1.0f - getInterpolation(1.0f - t)
+    }
+
+    object BounceEaseInOut : Interpolations() {
+        override fun getInterpolation(t: Float): Float = if (t < 0.5) {
+            0.5f * getInterpolation(t * 2.0f)
+        } else {
+            0.5f * getInterpolation(t * 2.0f - 1.0f) + 0.5f
+        }
+    }
+
+    object SmoothStep : Interpolations() {
+        override fun getInterpolation(t: Float): Float = t * t * (3 - 2 * t)
+    }
 }
